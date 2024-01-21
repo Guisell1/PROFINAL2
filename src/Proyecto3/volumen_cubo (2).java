e91f0a7e61f03109e51b1c9bdd919fa10d9b4014
yajai <yajai@Guiisell>
2024-01-21 21:29:11 +0100

    paso

---
+++ src/Proyecto3/volumen_cubo (2).java
@@ -0,0 +1,29 @@
+package UD2;
+
+import java.util.Scanner;
+
+public class volumen_cubo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Introduce el lado del cubo: ");
+		double lado = introEntero();		
+		double resultadovolumencubo = calcularVolumencubo(lado);
+		verResultado(lado,resultadovolumencubo);		
+	}
+	
+	public static double introEntero() {
+		Scanner entrada = new Scanner(System.in);		
+		return entrada.nextDouble();		
+	}
+	
+	public static double calcularVolumencubo(double lado) {
+		return lado * lado * lado;
+	}
+	
+	public static void verResultado(double lado,double resultadovolumencubo) {
+		System.out.println("El volumen del cubo es: " + resultadovolumencubo);
+	}
+
+	
+}
